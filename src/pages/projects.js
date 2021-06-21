f9564a390aa00936a57a4c82c4668f959f39a2ef
Matthew Sykes <mnsykes@gmail.com>
2021-06-21 15:18:53 -0400

    deleted unused modules

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,5 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-// import Seeds from "../components/Seeds";
-// import Ecbl from "../components/Ecbl";
-// import Diary from "../components/Diary";
 import ProjectCard from "../components/ProjectCard";
 import { card, imgArea, descArea, cardImage, gitLink } from "../styles/projects.module.scss";
 
